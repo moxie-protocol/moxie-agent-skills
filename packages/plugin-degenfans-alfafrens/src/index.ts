28639623a8dd831e83e83d1c072a18d799ec6a4d
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2025-04-07 10:37:18 +0400

    add gas usage action for testing

--- packages/plugin-degenfans-alfafrens/src/index.ts
+++ packages/plugin-degenfans-alfafrens/src/index.ts
@@ -1,14 +1,14 @@
 import type { Plugin } from "@moxie-protocol/core";
 
-import {  stakingConsultantAction } from "./actions/stakingConsultantAction";
-import { gasUsageTemplate } from "./templates";
+import { stakingConsultantAction } from "./actions/stakingConsultantAction";
 import { gasUsageAction } from "./actions/gasUsageAction";
 import { infoAction } from "./actions/infoAction";
 
 const degenfansAlfaFrensPlugin: Plugin = {
     name: "AlfaFrens",
-    description: "AlfaFrens agent for several informations, like staking, gas usage etc.!",
-    actions: [stakingConsultantAction,infoAction], //gasUsageAction
+    description:
+        "AlfaFrens agent for several informations, like staking, gas usage etc.!",
+    actions: [stakingConsultantAction, infoAction, gasUsageAction],
     providers: [],
     evaluators: [],
     services: [],
