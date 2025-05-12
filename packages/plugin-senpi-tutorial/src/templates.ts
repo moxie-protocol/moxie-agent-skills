d0592b23e16c0215e333353129e27ceae4234fc5
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2025-05-12 14:30:00 +0800

    finish tutorial skill

---
+++ packages/plugin-senpi-tutorial/src/templates.ts
@@ -0,0 +1,39 @@
+export const tutorialTemplate = `
+Your job is to provide the appropriate tutorial based on the user's request for help based on the historical messages.
+
+Here are the available tutorials and their corresponding examples:
+
+1. Getting Started Tutorial
+
+- This tutorial is for users who are new to Senpi and want to learn how to use it.
+- It's also helpful for users who might have trouble using certain features of Senpi.
+- It's also helpful for users who want to learn how to use Senpi to its full potential.
+
+Link: https://youtu.be/lfSHJE_6k-4?si=sQcRfKwHBjELA2YJ
+
+2. Autonomous Trade Tutorial
+
+- This tutorial is for users who want to learn how to set up an autonomous trade on Senpi.
+- It's also helpful for users who have trouble setting up their autonomous trade.
+
+Link: https://youtu.be/uElZko09rOg?si=BP4NYWgKm5MUHqV0x
+
+3. Token Research Tutorial
+
+- This tutorial is for users who want to learn how to research tokens using some Skills provided by Senpi.
+- It's also helpful for users who have trouble researching tokens on Senpi.
+
+Link: https://youtu.be/VigFMPzPmjQ?si=cAtVfHt-RrC86eKs
+
+4. Limit Order Tutorial
+
+- This tutorial is for users who want to learn how to set up a limit order on Senpi.
+- It's also helpful for users who have trouble setting up their limit order.
+
+Link: https://youtu.be/kYIPitELtKY?si=wBeQ2SEjHU1QxOaI
+
+If you don't find any of the tutorials relevant to the user's request, just say "Sorry, I don't have any tutorials for that request. Please try again later."
+
+Here are the recent user messages for context:
+{{recentMessages}}
+`;
