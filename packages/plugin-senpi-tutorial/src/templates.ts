5af78333a344aeff8a85f2c2991c04f90e978cf6
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2025-05-12 15:32:11 +0800

    change link to env variable

--- packages/plugin-senpi-tutorial/src/templates.ts
+++ packages/plugin-senpi-tutorial/src/templates.ts
@@ -9,28 +9,28 @@ Here are the available tutorials and their corresponding examples:
 - It's also helpful for users who might have trouble using certain features of Senpi.
 - It's also helpful for users who want to learn how to use Senpi to its full potential.
 
-Link: https://youtu.be/lfSHJE_6k-4?si=sQcRfKwHBjELA2YJ
+Link: ${process.env.GET_STARTED_TUTORIAL_URL}
 
 2. Autonomous Trade Tutorial
 
 - This tutorial is for users who want to learn how to set up an autonomous trade on Senpi.
 - It's also helpful for users who have trouble setting up their autonomous trade.
 
-Link: https://youtu.be/uElZko09rOg?si=BP4NYWgKm5MUHqV0x
+Link: ${process.env.AUTONOMOUS_TRADE_TUTORIAL_URL}
 
 3. Token Research Tutorial
 
 - This tutorial is for users who want to learn how to research tokens using some Skills provided by Senpi.
 - It's also helpful for users who have trouble researching tokens on Senpi.
 
-Link: https://youtu.be/VigFMPzPmjQ?si=cAtVfHt-RrC86eKs
+Link: ${process.env.TOKEN_RESEARCH_TUTORIAL_URL}
 
 4. Limit Order Tutorial
 
 - This tutorial is for users who want to learn how to set up a limit order on Senpi.
 - It's also helpful for users who have trouble setting up their limit order.
 
-Link: https://youtu.be/kYIPitELtKY?si=wBeQ2SEjHU1QxOaI
+Link: ${process.env.LIMIT_ORDER_TUTORIAL_URL}
 
 If you don't find any of the tutorials relevant to the user's request, just say "Sorry, I don't have any tutorials for that request. Please try again later."
 
